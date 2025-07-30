581f3763ac6d064efb70456926e0c268211be54e
Betzabet Mera <betzabetmera@gmail.com>
2025-07-30 08:09:35 -0600

    correción de todos los scripts de productos

--- src/main/java/org/generation/ch55Spring/controller/ProductosController.java
+++ src/main/java/org/generation/ch55Spring/controller/ProductosController.java
@@ -2,49 +2,48 @@ package org.generation.ch55Spring.controller;
 
 
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
-import org.generation.ch55Spring.model.Products;
-import org.generation.ch55Spring.service.ProductsService;
+import org.generation.ch55Spring.model.Productos;
+import org.generation.ch55Spring.service.ProductosService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(path="/api/products")// http://localhost:8080/api/products
+@RequestMapping(path="/api/productos")// http://localhost:8080/api/productos
 //lombok
 @AllArgsConstructor
-public class ProductsController {
+public class ProductosController {
     //inyeccion de dependencias
-    private ProductsService productsService;
+    private ProductosService productosService;
 
     // peticion get
     @GetMapping //url a la que se hace la peticion http://localhost:8080/api/products
-    public List<Products> getAllProducts(){
-        return this.productsService.getAllProducts();
+    public List<Productos> getAllProducts(){
+        return this.productosService.getAllProducts();
     }
 
     //peticion get para obtener producto por id
     @GetMapping(path = "{productId}") // http://localhost:8080/api/products/id  http://localhost:8080/api/products/2
-    public Products getProductById(@PathVariable("productId")Long id){
-        return this.productsService.getProductById(id);
+    public Productos getProductById(@PathVariable("productId")Long id){
+        return this.productosService.getProductById(id);
     }
 
     //peticion post @RequestBody indica que la peticion tiene un body con informacion
     @PostMapping //url a la que se hace la peticion http://localhost:8080/api/products , solo que aqui el metodo a usar POST
-    public Products addProduct(@RequestBody Products products){
-        return this.productsService.addProduct(products);
+    public Productos addProduct(@RequestBody Productos products){
+        return this.productosService.addProduct(products);
     }
 
     // peticion Delete
     @DeleteMapping(path = "{productId}")// http://localhost:8080/api/products/id  http://localhost:8080/api/products/2 pero con peticion tipo delete
-    public Products deleteProductById(@PathVariable("productId")Long id){
-        return this.productsService.deleteProductById(id);
+    public Productos deleteProductById(@PathVariable("productId")Long id){
+        return this.productosService.deleteProductById(id);
     }
 
     //peticion put
     @PutMapping(path = "{productId}")// http://localhost:8080/api/products/id  http://localhost:8080/api/products/2 peticion put y un body
-    public Products updateProductById(@PathVariable("productId")Long id, @RequestBody Products productUpdated){
-        return this.productsService.updateProductById(id, productUpdated);
+    public Productos updateProductById(@PathVariable("productId")Long id, @RequestBody Productos productUpdated){
+        return this.productosService.updateProductById(id, productUpdated);
     }
 
 }
